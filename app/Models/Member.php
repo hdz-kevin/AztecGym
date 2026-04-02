328feba8e1e2ce93807ba0f59bd433abd61e6282
Tomas Hernandez <kevinhdez592@gmail.com>
2026-04-01 22:15:06 -0600

    generate a unique random code before inserting a new member

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * @inheritdoc
+     */
+    protected static function booted(): void
+    {
+        // Generate a unique code for the member before inserting
+        static::creating(function (Member $member) {
+            do {
+                $code = (string) random_int(10000, 99999);
+            } while (self::where('code', $code)->exists());
+
+            $member->code = $code;
+        });
+    }
 }

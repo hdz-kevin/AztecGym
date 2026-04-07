dadc474bf073996299726273e231d09f5070b5fe
Tomas Hernandez <kevinhdez592@gmail.com>
2026-04-06 22:19:46 -0600

    work with Period start_date and end_date: - cast to datetime - apply format before saving (create or update)

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -9,6 +9,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 #[Fillable(['membership_id', 'duration_id', 'start_date', 'end_date', 'price_paid'])]
 class Period extends Model
 {
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+
+    /**
+     * The "booted" method of the model.
+     *  
+     * @return void
+     */
+    protected static function booted(): void
+    {
+        // Apply the format to the start and end dates before saving
+        static::saving(function (Period $period) {
+            $period->start_date = $period->start_date->startOfDay();
+
+            $period->end_date = $period->end_date->startOfDay();
+        });
+    }
+
     /**
      * Get the duration assigned to the period.
      */

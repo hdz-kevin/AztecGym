bbdbeea41ca7fc837f0d4f4db20ae1aa05e894c0
Tomas Hernandez <kevinhdez592@gmail.com>
2026-04-11 15:27:50 -0600

    define MembershipType and Memberhip typescript types

--- resources/js/types/models.ts
+++ resources/js/types/models.ts
@@ -1,9 +1,22 @@
+export type MembershipType = {
+    id: number;
+    name: string;
+}
+
+export type Membership = {
+    id: number;
+    member_id: number;
+    membership_type_id: number;
+    membership_type: MembershipType;
+}
+
 export type Member = {
     id: number;
     name: string;
     code: string;
-    gender: string,
-    birth_date: string,
-    photo: string,
-    status?: string,
+    gender: string;
+    birth_date: string;
+    photo: string;
+    status?: string;
+    current_membership?: Membership | null;
 }
